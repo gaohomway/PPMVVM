7db240d3a61d82a553d0f4674e643099b209d3e4
weifans <duanhaisi@gmail.com>
2015-12-21 12:51:47 +0800

    project init

---
+++ PPMVVM/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  PPMVVM
+//
+//  Created by weifans on 15/12/21.
+//  Copyright © 2015年 weifans. All rights reserved.
+//
+
+import Cocoa
+
+class UserModel: NSObject {
+
+}
---
+++ PPMVVM/config.h
@@ -0,0 +1,13 @@
+//
+//  config.h
+//  PPMVVM
+//
+//  Created by weifans on 15/12/21.
+//  Copyright © 2015年 weifans. All rights reserved.
+//
+
+#ifndef config_h
+#define config_h
+
+
+#endif /* config_h */
